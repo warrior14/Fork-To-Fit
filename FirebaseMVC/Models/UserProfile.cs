f9fa63233a69301fce93c4320dba627c49ead39a
warrior14 <ironaddicts14@gmail.com>
2021-10-21 19:52:24 -0500

    finished adding properties to Food.cs and UserProfile.cs

--- FirebaseMVC/Models/UserProfile.cs
+++ FirebaseMVC/Models/UserProfile.cs
@@ -1,9 +1,23 @@
-﻿namespace ForkToFit.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ForkToFit.Models
 {
     public class UserProfile
     {
         public int Id { get; set; }
+
+        public string Name { get; set; }
         public string FirebaseUserId { get; set; }
         public string Email { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int BmrInfo { get; set; }
+
     }
 }
