5759a6b9af4b040ee6f56c787e3cb8707f853ff3
warrior14 <ironaddicts14@gmail.com>
2021-10-24 15:31:57 -0500

    created all the models and files needed

--- FirebaseMVC/Models/DisplayedFood.cs
+++ FirebaseMVC/Models/DisplayedFood.cs
@@ -5,10 +5,20 @@ using System.Threading.Tasks;
 
 namespace ForkToFit.Models
 {
-    public class MealTime
+    public class DisplayedFood
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public int ServingSize { get; set; }
+
+        public int Calories { get; set; } 
+
+        public string Description { get; set; }
+
+        public int MacroCategoryId { get; set; }
+
+
     }
 }
