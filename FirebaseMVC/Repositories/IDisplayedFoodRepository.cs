edb94fe8ffae99da00086ec5c2ad067d27d84b77
warrior14 <ironaddicts14@gmail.com>
2021-10-25 14:26:23 -0500

    created a MealPlanController.cs

--- FirebaseMVC/Repositories/IDisplayedFoodRepository.cs
+++ FirebaseMVC/Repositories/IDisplayedFoodRepository.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using ForkToFit.Models;
 
+
+
 namespace ForkToFit.Repositories
 {
     public interface IDisplayedFoodRepository
--- FirebaseMVC/Repositories/MealPlan.cs
+++ FirebaseMVC/Repositories/MealPlan.cs
@@ -2,5 +2,10 @@
 {
     public class MealPlan
     {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public int UserProfileId { get; internal set; }
+        public int MealPlanTypeId { get; internal set; }
+        public string CalorieTracker { get; internal set; }
     }
 }
