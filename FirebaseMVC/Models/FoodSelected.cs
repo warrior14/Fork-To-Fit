c4d5906007141e6368f58224b5247e2a02dc0081
warrior14 <ironaddicts14@gmail.com>
2021-10-27 13:44:00 -0500

    created a FoodSelected.cs and creating a details view for displaying all foods in a meal plan

---
+++ FirebaseMVC/Models/FoodSelected.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ForkToFit.Models
+{
+    public class FoodSelected
+    {
+        public int Id { get; set; }
+
+
+        public int MealPlanId { get; set; }
+
+        public int DayCategoryId { get; set; }
+
+        public int MealTimeId { get; set; }
+
+        public int DisplayedFoodId { get; set; }
+
+
+    }
+}
